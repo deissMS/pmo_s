84fa13849f098d6cd06e0fa5d526f12617a6fd60
Dirección de Estrategias de Integración del Sistema de Salud <138637441+deissMS@users.noreply.github.com>
2023-07-06 23:07:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('https://deissMS.github.io/pmo_s/consolidado.json')
+    fetch('https://deissms.github.io/pmo_m/consolidado.json')
     .then(response => response.json())
     .then(data => {
         let categories = [...new Set(data.map(item => item.categoria))];
@@ -49,7 +49,56 @@ document.addEventListener('DOMContentLoaded', (event) => {
                            '<p class="resultado"><b>Nivel de cobertura: ' + coberturaText + '</b></p>' +
                            '<p class="resultado">Recomendaciones de uso: ' + obj.recomendaciones + '</p>';
                 });
-                document.getElementById('texto-seccion').innerHTML = `<h2 class="titulo-resultado">Resultado de la búsqueda: ${resultado.length} resultado/s encontrado/s</h2>` + coberturas.join('<hr>');
+
+                var tituloResultado = resultado.length === 1 ? "Resultado de la búsqueda: 1 prestación encontrada" : "Resultado de la búsqueda: " + resultado.length + " prestaciones encontradas";
+
+                document.getElementById('texto-seccion').innerHTML = `
+                  <div class="acciones">
+                    <button id="descargar-resultados" class="boton-accion">Descargar Resultados</button>
+                    <button id="descargar-consolidado" class="boton-accion">Descargar Canasta Prestacional</button>
+                    <a href="https://www.argentina.gob.ar/normativa/nacional/resolución-201-2002-73649/actualizacion" target="_blank" class="boton-accion">Ver legislación</a>
+                  </div>
+                  <h2 class="titulo-resultado">${tituloResultado}</h2>
+                  ` + coberturas.join('<hr>');
+
+                document.getElementById('descargar-consolidado').addEventListener('click', function() {
+                  window.location.href = 'data/consolidado.xlsx'; // Cambiar la ruta del archivo si es necesario
+                });
+
+                document.getElementById('descargar-resultados').addEventListener('click', function() {
+                  /* Crear un objeto de libro de trabajo */
+                  var wb = XLSX.utils.book_new();
+                  wb.Props = {
+                      Title: "Resultados de la búsqueda",
+                      Author: "Tu nombre",
+                      CreatedDate: new Date()
+                  };
+
+                  /* Crear una hoja de cálculo */
+                  wb.SheetNames.push("Resultados");
+
+                  /* Convertir los datos a formato de hoja de cálculo */
+                  var ws_data = resultado.map(function(obj) {
+                      return [
+                          obj.nombre,
+                          obj.categoria,
+                          obj.subcategoria,
+                          obj.norma,
+                          isNumeric(obj.cobertura) ? (obj.cobertura * 100) + '%' : obj.cobertura,
+                          obj.recomendaciones
+                      ];
+                  });
+                  ws_data.unshift(["Nombre", "Categoría", "Subcategoría", "Normativa", "Nivel de cobertura", "Recomendaciones"]); // Añadir encabezados de columna
+
+                  var ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+                  /* Añadir la hoja de cálculo al libro de trabajo */
+                  wb.Sheets["Resultados"] = ws;
+
+                  /* Guardar el libro de trabajo como archivo XLSX */
+                  var wbout = XLSX.write(wb, {bookType:'xlsx', type: 'binary'});
+                  saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'resultados.xlsx');
+                });
             } else {
                 alert('No se encontró el valor buscado');
             }
@@ -60,4 +109,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function s2ab(s) { 
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;    
 }
